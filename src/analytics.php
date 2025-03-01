bcc4d506b814dbe96a25dbb70947ed5a92a54425
Miro Gohacki <mirogohacki@gmail.com>
2025-03-01 13:13:23 -0500

    some changes

--- src/analytics.php
+++ src/analytics.php
@@ -1,15 +1,5 @@
 <?php include 'top.php'?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<body class="analytics">
 
     <!-- All  -->
     <div class='graph'>
