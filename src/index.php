50da213761006934d91eb77673d8f4ef9590985f
NicolasFay <njfay@uvm.edu>
2025-03-01 10:37:36 -0500

    added header and search bar with some filters

--- src/index.php
+++ src/index.php
@@ -1,6 +1,29 @@
 <!-- index.php -->
+
 <?php include 'top.php'?>
-<main>
-    <h2>Home</h2>
+<main class = "index">
+    <h1>Rally Cat's Cupboard</h1>
+<!--SEARCH BAR -->
+    <section class = "search">
+        <input type="text" placeholder="Search..">
+        <select id="filterCategory">
+
+            <option value="">All Categories</option>
+
+            <option value="Fruit">Fruit</option>
+            <option value="Vegetable">Vegetable</option>
+            <option value="Protein">Protein</option>
+            <option value="Milk Allergy">Milk Allergy</option>
+            <option value="Peanut Allergy">Peanut Allergy</option>
+            <option value="Treenut Allergy">Treenut Allergy</option>
+            <option value="Gluten Allergy">Gluten Allergy</option>
+
+        </select>
+
+
+    </section>
+
+<!-- ITEMS -->
+
 </main>
 <?php include 'footer.php' ?>
