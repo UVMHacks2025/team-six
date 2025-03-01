53ccd3e5af10b14e8bfcbeba9d82f43c1bae6bef
Miro Gohacki <mirogohacki@gmail.com>
2025-03-01 10:04:04 -0500

    addign to gitignore

--- src/nav.php
+++ src/nav.php
@@ -1,6 +1,31 @@
 <!-- nav.php -->
-<nav>
-    <p>nav buttons here</p>
+<nav class="navbar">
+        <div class="container">
+            <h1 class="logo"><a href="{{ url_for('index') }}"><img src="static/images/logo.jpg" alt="Logo" class="logo-image"></a></h1>
+            <div class="user-section">
+                {% if current_user.is_authenticated and current_user.AgentID > 0 %}
+                    <p class="welcome">Welcome, {{ current_user.FirstName }}!</p>
+                    <a href="{{ url_for('index') }}">Home</a>
+                    <a href="{{ url_for('add_listing') }}">Add Listing</a>
+                    <a href="{{ url_for('logout') }}">Logout</a>
+                    <a href="{{ url_for('saved_listings') }}">Saved Listings</a>
+                    <a href="{{ url_for('show_graphs') }}">View Trends</a>
+                    <a href="{{ url_for('statistics') }}">View Statistics</a>
+                {% elif current_user.is_authenticated %}
+                    <a href="{{ url_for('index') }}">Home</a>
+                    <p class="welcome">Welcome, {{ current_user.FirstName }}!</p>
+                    <a href="{{ url_for('logout') }}">Logout</a>
+                    <a href="{{ url_for('saved_listings') }}">Saved Listings</a>
+                    <a href="{{ url_for('show_graphs') }}">View Trends</a>
+                    <a href="{{ url_for('statistics') }}">View Statistics</a>
+                {% else %}
+                    <a href="{{ url_for('index') }}">Home</a>
+                    <a href="{{ url_for('login') }}">Login</a>
+                    <a href="{{ url_for('show_graphs') }}">View Trends</a>
+                    <a href="{{ url_for('statistics') }}">View Statistics</a>
+                {% endif %}
+            </div>
+        </div>
     <?php
     ?>
 </nav>
